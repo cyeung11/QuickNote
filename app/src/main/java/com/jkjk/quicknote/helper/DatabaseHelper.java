7d90831d47a3da2d5c08515858fe20509954c2e6
cyeung11 <38922978+cyeung11@users.noreply.github.com>
2018-08-29 10:32:17 +0800

    Update DatabaseHelper.java

--- app/src/main/java/com/jkjk/quicknote/helper/DatabaseHelper.java
+++ app/src/main/java/com/jkjk/quicknote/helper/DatabaseHelper.java
@@ -13,49 +13,49 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public final static String DATABASE_NAME = "note";
     public final static int CURRENT_DB_VER = 8;
     private final static String CREATE_STRING = "CREATE TABLE IF NOT EXISTS " + DATABASE_NAME +
-            " (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL" +
-            ", event_time INTEGER NOT NULL, starred INTEGER NOT NULL DEFAULT '0', type INTEGER NOT NULL" +
-            ", urgency INTEGER NOT NULL DEFAULT '0', done INTEGER NOT NULL DEFAULT '0', reminder_time INTEGER NOT NULL DEFAULT '0'" +
-            ", repeat_interval INTEGER NOT NULL DEFAULT '0', lat_lng TEXT, place_name TEXT)";
+            " (" + dbColumn[0] + " INTEGER PRIMARY KEY AUTOINCREMENT, " + dbColumn[1] + " TEXT NOT NULL, " + dbColumn[2] + " TEXT NOT NULL" +
+            ", " + dbColumn[3] + " INTEGER NOT NULL, " + dbColumn[4] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[5] + " INTEGER NOT NULL" +
+            ", " + dbColumn[6] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[7] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[8] + " INTEGER NOT NULL DEFAULT '0'" +
+            ", " + dbColumn[9] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[10] + " TEXT, " + dbColumn[11] + " TEXT)";
 
     private static final String DATABASE_ALTER_V2 = "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN starred INTEGER;";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[4] + " INTEGER;";
 
     private static final String DATABASE_ALTER_V3_1 = "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN type INTEGER NOT NULL DEFAULT '0';";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[5] + " INTEGER NOT NULL DEFAULT '0';";
     private static final String DATABASE_ALTER_V3_2 = "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN urgency INTEGER NOT NULL DEFAULT '0';";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[6] + " INTEGER NOT NULL DEFAULT '0';";
     private static final String DATABASE_ALTER_V3_3 = "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN done INTEGER NOT NULL DEFAULT '0';";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[7] + " INTEGER NOT NULL DEFAULT '0';";
 
     private static final String DATABASE_ALTER_V4_1 = "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN event_time INTEGER NOT NULL DEFAULT '0';";
-    private static final String DATABASE_ALTER_V4_2 = "CREATE TEMPORARY TABLE note_backup (_id INTEGER PRIMARY KEY AUTOINCREMENT" +
-            ", title TEXT NOT NULL, content TEXT NOT NULL, event_time INTEGER NOT NULL, starred INTEGER NOT NULL DEFAULT '0'" +
-            ", type INTEGER NOT NULL, urgency INTEGER NOT NULL DEFAULT '0', done INTEGER NOT NULL DEFAULT '0')";
-    private static final String DATABASE_ALTER_V4_3 = "INSERT INTO note_backup SELECT _id, title, content, event_time, starred, type, urgency, done FROM "
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[3] + " INTEGER NOT NULL DEFAULT '0';";
+    private static final String DATABASE_ALTER_V4_2 = "CREATE TEMPORARY TABLE note_backup (" + dbColumn[0] + " INTEGER PRIMARY KEY AUTOINCREMENT" +
+            ", " + dbColumn[1] + " TEXT NOT NULL, " + dbColumn[2] + " TEXT NOT NULL, " + dbColumn[3] + " INTEGER NOT NULL, " + dbColumn[4] + " INTEGER NOT NULL DEFAULT '0'" +
+            ", " + dbColumn[5] + " INTEGER NOT NULL, " + dbColumn[6] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[7] + " INTEGER NOT NULL DEFAULT '0')";
+    private static final String DATABASE_ALTER_V4_3 = "INSERT INTO note_backup SELECT " + dbColumn[0] + ", " + dbColumn[1] + ", " + dbColumn[2] + ", " + dbColumn[3] + ", " + dbColumn[4] + ", " + dbColumn[5] + ", " + dbColumn[6] + ", " + dbColumn[7] + " FROM "
             + DATABASE_NAME + ";";
     private static final String DATABASE_ALTER_V4_4 = "DROP TABLE "
             + DATABASE_NAME + ";";
     private static final String DATABASE_ALTER_V4_5 = "CREATE TABLE IF NOT EXISTS " + DATABASE_NAME +
-            " (_id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, content TEXT NOT NULL" +
-            ", event_time INTEGER NOT NULL, starred INTEGER NOT NULL DEFAULT '0', type INTEGER NOT NULL" +
-            ", urgency INTEGER NOT NULL DEFAULT '0', done INTEGER NOT NULL DEFAULT '0')";
+            " (" + dbColumn[0] + " INTEGER PRIMARY KEY AUTOINCREMENT, " + dbColumn[1] + " TEXT NOT NULL, " + dbColumn[2] + " TEXT NOT NULL" +
+            ", " + dbColumn[3] + " INTEGER NOT NULL, " + dbColumn[4] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[5] + " INTEGER NOT NULL" +
+            ", " + dbColumn[6] + " INTEGER NOT NULL DEFAULT '0', " + dbColumn[7] + " INTEGER NOT NULL DEFAULT '0')";
     private static final String DATABASE_ALTER_V4_6 = "INSERT INTO " + DATABASE_NAME
-            + " SELECT _id, title, content, event_time, starred, type, urgency, done FROM note_backup;";
+            + " SELECT " + dbColumn[0] + ", " + dbColumn[1] + ", " + dbColumn[2] + ", " + dbColumn[3] + ", " + dbColumn[4] + ", " + dbColumn[5] + ", " + dbColumn[6] + ", " + dbColumn[7] + " FROM note_backup;";
     private static final String DATABASE_ALTER_V4_7 = "DROP TABLE note_backup;";
 
     private static final String DATABASE_ALTER_V5 =  "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN reminder_time INTEGER NOT NULL DEFAULT '0';";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[8] + " INTEGER NOT NULL DEFAULT '0';";
 
     private static final String DATABASE_ALTER_V6 =  "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN repeat_interval INTEGER NOT NULL DEFAULT '0';";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[9] + " INTEGER NOT NULL DEFAULT '0';";
 
     private static final String DATABASE_ALTER_V7 =  "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN lat_lng TEXT;";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[10] + " TEXT;";
     
     private static final String DATABASE_ALTER_V8 =  "ALTER TABLE "
-            + DATABASE_NAME + " ADD COLUMN place_name TEXT;";
+            + DATABASE_NAME + " ADD COLUMN " + dbColumn[11] + " TEXT;";
 
     public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
         super(context,name,factory,version);
